c7a901d399a6fa152b72ba135b9f154dc6741d1c
ssssss <xxxxxx@xxxx.com>
2016-08-12 21:19:03 -0500

    ejercicios devices
    ejercicios devices

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,108 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-device.device",
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "id": "cordova-plugin-statusbar.statusbar",
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "id": "ionic-plugin-keyboard.keyboard",
+        "file": "plugins/ionic-plugin-keyboard/www/android/keyboard.js",
+        "pluginId": "ionic-plugin-keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ],
+        "runs": true
+    },
+    {
+        "id": "cordova-plugin-device-motion.Acceleration",
+        "file": "plugins/cordova-plugin-device-motion/www/Acceleration.js",
+        "pluginId": "cordova-plugin-device-motion",
+        "clobbers": [
+            "Acceleration"
+        ]
+    },
+    {
+        "id": "cordova-plugin-device-motion.accelerometer",
+        "file": "plugins/cordova-plugin-device-motion/www/accelerometer.js",
+        "pluginId": "cordova-plugin-device-motion",
+        "clobbers": [
+            "navigator.accelerometer"
+        ]
+    },
+    {
+        "id": "cordova-plugin-network-information.network",
+        "file": "plugins/cordova-plugin-network-information/www/network.js",
+        "pluginId": "cordova-plugin-network-information",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "id": "cordova-plugin-network-information.Connection",
+        "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+        "pluginId": "cordova-plugin-network-information",
+        "clobbers": [
+            "Connection"
+        ]
+    },
+    {
+        "id": "cordova-plugin-device-orientation.CompassError",
+        "file": "plugins/cordova-plugin-device-orientation/www/CompassError.js",
+        "pluginId": "cordova-plugin-device-orientation",
+        "clobbers": [
+            "CompassError"
+        ]
+    },
+    {
+        "id": "cordova-plugin-device-orientation.CompassHeading",
+        "file": "plugins/cordova-plugin-device-orientation/www/CompassHeading.js",
+        "pluginId": "cordova-plugin-device-orientation",
+        "clobbers": [
+            "CompassHeading"
+        ]
+    },
+    {
+        "id": "cordova-plugin-device-orientation.compass",
+        "file": "plugins/cordova-plugin-device-orientation/www/compass.js",
+        "pluginId": "cordova-plugin-device-orientation",
+        "clobbers": [
+            "navigator.compass"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-console": "1.0.3",
+    "cordova-plugin-device": "1.1.2",
+    "cordova-plugin-splashscreen": "3.2.2",
+    "cordova-plugin-statusbar": "2.1.3",
+    "cordova-plugin-whitelist": "1.2.2",
+    "ionic-plugin-keyboard": "2.2.1",
+    "cordova-plugin-device-motion": "1.2.1",
+    "cordova-plugin-network-information": "1.2.1",
+    "cordova-plugin-device-orientation": "1.0.3"
+};
+// BOTTOM OF METADATA
+});
---
+++ platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,64 @@
+angular.module('app.controllers', ['ngCordova'])
+     
+.controller('dispositvoCtrl', ['$scope', '$stateParams', '$cordovaDevice',  // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams, $cordovaDevice) {
+    $scope.DeviceInformation = function(){
+	    $scope.device = $cordovaDevice.getDevice();
+        $scope.cordova = $cordovaDevice.getCordova();
+    	$scope.model = $cordovaDevice.getModel();
+    	$scope.platform =  $cordovaDevice.getPlatform();
+    	$scope.uuid = $cordovaDevice.getUUID();
+    	$scope.version = $cordovaDevice.getVersion();
+	}
+}])
+   
+.controller('acelerometroCtrl', ['$scope', '$stateParams', '$cordovaDeviceMotion', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams, $cordovaDeviceMotion) {
+       $scope.getCurrentDeviceMotion = function(){
+		$cordovaDeviceMotion.getCurrentAcceleration().then(function(result) {
+		      $scope.x = result.x;
+		      $scope.y = result.y;
+		      $scope.z = result.z;
+		      $scope.timeStamp = result.timestamp;
+	    }, function(err) {
+	      // An error occurred. Show a message to the user
+          alert('Algo salio mal');
+	    });
+     }
+
+}])
+   
+.controller('redCtrl', ['$scope', '$stateParams', '$cordovaNetwork',  // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams, $cordovaNetwork) {
+    $scope.getNetwork = function(){
+       $scope.tipo = $cordovaNetwork.getNetwork();
+       $scope.isOnline = $cordovaNetwork.isOnline();
+      $scope.isOffline = $cordovaNetwork.isOffline();
+    }
+
+}])
+   
+.controller('brujulaCtrl', ['$scope', '$stateParams', '$cordovaDeviceOrientation', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams, $cordovaDeviceOrientation) {
+     $scope.getOrientacion = function(){
+        $cordovaDeviceOrientation.getCurrentHeading().then(function(result) {
+         $scope.magneticHeading = result.magneticHeading;
+       $scope.trueHeading = result.trueHeading;
+       $scope.accuracy = result.headingAccuracy;
+        $scope.timeStamp = result.timestamp;
+        }, function(err) {
+        // An error occurred
+         alert('Algo salio mal');
+        });
+    }
+
+}])
+ 
